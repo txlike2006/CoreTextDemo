c005c5f70d89f4aab32934d9e2e75ec7da6a80e3
ke.li <ke.li@kelideMacBook-Pro.local>
2018-01-30 10:31:09 +0800

    first commit

---
+++ CoreTextDemo/CoreTextDemo/CoreTextDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreTextDemo
+//
+//  Created by ke.li on 2018/1/30.
+//  Copyright © 2018年 ke.li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
